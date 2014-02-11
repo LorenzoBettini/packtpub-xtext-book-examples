0636611415d6dee389e4fad2a6114879d80d1564
Lorenzo Bettini <bettini@dsi.unifi.it>
2014-02-11 09:29:03 +0100

    removed warnings in ui projects
    Change-Id: I3d2425ffd8fc0892f79ce280539c64c99e90da2d

--- org.example.smalljava.ui/src-gen/org/example/smalljava/ui/contentassist/antlr/PartialSmallJavaContentAssistParser.java
+++ org.example.smalljava.ui/src-gen/org/example/smalljava/ui/contentassist/antlr/PartialSmallJavaContentAssistParser.java
@@ -15,6 +15,7 @@ import org.eclipse.xtext.util.PolymorphicDispatcher;
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
+@SuppressWarnings("restriction")
 public class PartialSmallJavaContentAssistParser extends SmallJavaParser implements IPartialContentAssistParser {
 
 	private AbstractRule rule;
